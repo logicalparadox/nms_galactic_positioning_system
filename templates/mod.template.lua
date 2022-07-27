7dc3faed2168976c01300bcf0d1c13cf58d56d21
logicalparadox <jake@alogicalparadox.com>
2022-07-27 14:23:25 -0400

    initial commit

---
+++ templates/mod.template.lua
@@ -0,0 +1,52 @@
+Author = "<%= env.Mod.Author %>"
+ModName = "<%= env.Mod.Name %>"
+BaseDescription = [[<%= (env.Mod.Description || '').trim() %>]]
+GameVersion = "<%= `${(env.Mod.GameVersion || '')}`.replace(new RegExp('\\.', 'g'), '-') %>"
+ModVersion = "<%= `${(env.Mod.Version || '')}`.replace(new RegExp('\\.', 'g'), '-') %>"
+
+--[[
+<% env.tables.forEach((file) => { %>
+<%= file.filename %>
+<% file.gcArray.forEach((e) => { %>- <%= e.id %>
+<% }); %><% }); %>
+--]]
+
+NMS_MOD_DEFINITION_CONTAINER = {
+
+["MOD_FILENAME"]				= Author.."_"..ModName.."_"..GameVersion.."_"..ModVersion..".pak",
+["MOD_DESCRIPTION"]			= BaseDescription,
+["MOD_AUTHOR"]					= Author,
+["NMS_VERSION"]					= GameVersion,
+
+["MODIFICATIONS"]	= {
+{["MBIN_CHANGE_TABLE"] = {
+
+  -- Dialog Removals
+  {["MBIN_FILE_SOURCE"] = "METADATA\\REALITY\\TABLES\\NMS_DIALOG_GCALIENPUZZLETABLE.MBIN",
+  ["EXML_CHANGE_TABLE"] = {
+    {["SPECIAL_KEY_WORDS"] = {"Id", "SIGNALSCANNER"}, ["REMOVE"] = "SECTION"},
+  }},
+
+  -- Injected Tables<% env.tables.forEach((table) => { %>
+  {["MBIN_FILE_SOURCE"] = "<%= table.MBIN_FILE_SOURCE %>",
+  ["EXML_CHANGE_TABLE"] = {
+    {["LINE_OFFSET"] = 0,
+    --<% if (table.SPECIAL_KEY_WORDS.length) { %>
+    ["SPECIAL_KEY_WORDS"] = {
+      --<% table.SPECIAL_KEY_WORDS.forEach((key) => { %>
+      "<%= key %>", --<% }); %>
+    },--<% } %>
+    --<% if (table.PRECEDING_KEY_WORDS.length) { %>
+    ["PRECEDING_KEY_WORDS"] = {
+      --<% table.PRECEDING_KEY_WORDS.forEach((key) => { %>
+      "<%= key %>", --<% }); %>
+    },--<% } %>
+    ["ADD"] = [[
+<%~ table.getFinalXml() %>      
+]]
+    },
+  }},
+
+  --<% }) %>
+
+}}}}
