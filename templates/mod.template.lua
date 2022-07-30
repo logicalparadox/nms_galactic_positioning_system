71920114429e307396a1ae8e7cf9cb519911032d
logicalparadox <jake@alogicalparadox.com>
2022-07-30 14:45:34 -0400

    navigator dialog fixes

--- templates/mod.template.lua
+++ templates/mod.template.lua
@@ -11,6 +11,38 @@ ModVersion = "<%= `${(env.Mod.Version || '')}`.replace(new RegExp('\\.', 'g'), '
 <% }); %><% }); %>
 --]]
 
+local navigatorDialog = [[        <Property value="GcAlienPuzzleOption.xml">
+          <Property name="Name" value="ALCH_SCAN_MENU_PQ_NAME"/>
+          <Property name="Text" value=""/>
+          <Property name="IsAlien" value="False"/>
+          <Property name="Cost" value=""/>
+          <Property name="Rewards"/>
+          <Property name="Mood" value="GcAlienMood.xml">
+            <Property name="Mood" value="Positive"/>
+          </Property>
+          <Property name="Prop" value="GcNPCPropTypes.xml">
+            <Property name="NPCProp" value="HoloSolarSystem"/>
+          </Property>
+          <Property name="KeepOpen" value="True"/>
+          <Property name="DisplayCost" value="True"/>
+          <Property name="TruncateCost" value="False"/>
+          <Property name="MarkInteractionComplete" value="True"/>
+          <Property name="NextInteraction" value="D_ALSCFB_HOME"/>
+          <Property name="SelectedOnBackOut" value="False"/>
+          <Property name="AudioEvent" value="GcAudioWwiseEvents.xml">
+            <Property name="AkEvent" value="INVALID_EVENT"/>
+          </Property>
+          <Property name="TitleOverride" value=""/>
+          <Property name="EnablingConditionTest" value="GcMissionConditionTest.xml">
+            <Property name="ConditionTest" value="AnyFalse"/>
+          </Property>
+          <Property name="EnablingConditions"/>
+          <Property name="EnablingConditionId" value=""/>
+          <Property name="WordCategory" value="GcWordCategoryTableEnum.xml">
+            <Property name="gcwordcategorytableEnum" value="MISC"/>
+          </Property>
+        </Property>]]
+
 NMS_MOD_DEFINITION_CONTAINER = {
 
 ["MOD_FILENAME"]				= Author.."_"..ModName.."_"..GameVersion.."_"..ModVersion..".pak",
@@ -29,13 +61,21 @@ NMS_MOD_DEFINITION_CONTAINER = {
       ["REMOVE"] = "SECTION",
     },
     {
-      ["SPECIAL_KEY_WORDS"] = {"Id", "FLEET_NAVIGATOR"}, 
-      ["REMOVE"] = "SECTION",
+      ["SPECIAL_KEY_WORDS"] = {"Id", "FLEET_NAVIGATOR", "Name", "NPC_NAVIGATOR_OPT_B"},
+      ["LINE_OFFSET"] = -2,
+      ["ADD"] = navigatorDialog,
       ["REPLACE_TYPE"] = "ALL"
     },
     {
-      ["SPECIAL_KEY_WORDS"] = {"Id", "FLEET_NAVIGATOR_NEW"}, 
-      ["REMOVE"] = "SECTION",
+      ["SPECIAL_KEY_WORDS"] = {"Id", "FLEET_NAVIGATOR_NEW", "Name", "NPC_NAVIGATOR_OPT_B"},
+      ["LINE_OFFSET"] = -2,
+      ["ADD"] = navigatorDialog,
+      ["REPLACE_TYPE"] = "ALL"
+    },
+    {
+      ["SPECIAL_KEY_WORDS"] = {"Id", "FLEET_NAVIGATOR_FULL_POST", "Name", "ALL_REQUEST_LEAVE"},
+      ["LINE_OFFSET"] = -2,
+      ["ADD"] = navigatorDialog,
       ["REPLACE_TYPE"] = "ALL"
     },
   }},
